1088ab19435d42cf1b59453ef90d405698e20972
Jeffry Gonzalez <jeff@hypertheory.com>
2021-02-22 11:52:43 -0500

    Mid Morning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MaterialModules } from './material-stuff';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForecastComponent } from './components/forecast/forecast.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ActionsComponent } from './components/actions/actions.component';
+import { InboxComponent } from './components/inbox/inbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashboardComponent,
+    ForecastComponent,
+    ProjectsComponent,
+    ActionsComponent,
+    InboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    ...MaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-stuff.ts
@@ -0,0 +1,17 @@
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips'
+export const MaterialModules: any[] = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatChipsModule
+]
